ef928bf3ab6dba780c7841762cf632219e6a2fb5
luisfelipedejesusm <luisfelipedejesusm@gmail.com>
2022-07-07 21:32:07 -0500

    lab10

---
+++ src/test/java/com/luisfelipedejesusm/eRegistrar/ERegistrarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisfelipedejesusm.eRegistrar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ERegistrarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
